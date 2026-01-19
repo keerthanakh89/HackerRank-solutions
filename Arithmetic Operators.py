2a4ce11e3e96176cce7c04a586202229e4181c03
KEERTHANA K H <keerthanakh01@gmail.com>
2026-01-19 20:24:02 +0530

    Create Arithmetic Operators.py

---
+++ Arithmetic Operators.py
@@ -0,0 +1,8 @@
+a = int(input().strip())
+b = int(input().strip())
+sum_result = a + b
+print(sum_result)
+difference_result = a - b
+print(difference_result)
+product_result = a * b
+print(product_result)
