ef97c328d249010d68a3dcd61a89dfd6b5b876c8
KEERTHANA K H <keerthanakh01@gmail.com>
2026-01-26 06:44:14 +0530

    Create String Formatting.py

---
+++ String Formatting.py
@@ -0,0 +1,19 @@
+def print_formatted(number):
+    # your code goes here
+    # Width is length of binary of number
+    width = len(bin(number)[2:])
+
+    for i in range(1, number + 1):
+        dec = str(i)
+        octal = oct(i)[2:]
+        hexa = hex(i)[2:].upper()
+        binary = bin(i)[2:]
+
+        print(dec.rjust(width),
+              octal.rjust(width),
+              hexa.rjust(width),
+              binary.rjust(width))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
