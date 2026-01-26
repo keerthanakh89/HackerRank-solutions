66439e9976364f0db9caa4a57aae3c29bd965adf
KEERTHANA K H <keerthanakh01@gmail.com>
2026-01-26 06:41:49 +0530

    Create Designer Door Mat.py

---
+++ Designer Door Mat.py
@@ -0,0 +1,13 @@
+# Read input
+N, M = map(int, input().split())
+
+# Top part
+for i in range(1, N, 2):
+    print((i * ".|.").center(M, "-"))
+
+# Middle part
+print("WELCOME".center(M, "-"))
+
+# Bottom part
+for i in range(N - 2, 0, -2):
+    print((i * ".|.").center(M, "-"))
