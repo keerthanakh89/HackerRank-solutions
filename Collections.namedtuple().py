9aa9c59306b6ac0a2089afb06de0e7d32f6e4185
KEERTHANA K H <keerthanakh01@gmail.com>
2026-02-08 15:02:44 +0530

    Create Collections.namedtuple().py

---
+++ Collections.namedtuple().py
@@ -0,0 +1,13 @@
+from collections import namedtuple
+
+n = int(input())
+fields = input().split()
+
+Student = namedtuple('Student', fields)
+
+total = 0
+for _ in range(n):
+    student = Student(*input().split())
+    total += int(student.MARKS)
+
+print(total / n)
