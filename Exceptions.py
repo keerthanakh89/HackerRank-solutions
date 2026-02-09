2bc815517a5b1708b02c167935b47bdb02cdbd09
KEERTHANA K H <keerthanakh01@gmail.com>
2026-02-09 18:42:16 +0530

    Update Exceptions.py

--- Exceptions.py
+++ Exceptions.py
@@ -4,5 +4,7 @@ for _ in range(t):
     try:
         a, b = map(int, input().split())
         print(a // b)
-    except Exception as e:
+    except ZeroDivisionError as e:
+        print("Error Code: integer division or modulo by zero")
+    except ValueError as e:
         print("Error Code:", e)
