87d70fadb7458107494df7f9d6137ed2ecd1eecb
KEERTHANA K H <keerthanakh01@gmail.com>
2026-03-01 22:00:33 +0530

    Update Company Logo.py

--- Company Logo.py
+++ Company Logo.py
@@ -5,7 +5,6 @@ if __name__ == '__main__':
 
     # Count character frequencies
     counter = Counter(s)
-
     # Sort by frequency descending, then alphabetically
     for char, count in sorted(counter.items(), key=lambda x: (-x[1], x[0]))[:3]:
         print(char, count)
