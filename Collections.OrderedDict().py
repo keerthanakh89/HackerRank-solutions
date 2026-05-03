08274a7b0f932302d838f2d395e91d9e9922e59f
KEERTHANA K H <keerthanakh01@gmail.com>
2026-05-03 22:59:31 +0530

    Remove unnecessary newline in OrderedDict script

--- Collections.OrderedDict().py
+++ Collections.OrderedDict().py
@@ -7,6 +7,5 @@ for _ in range(n):
     name, price = input().rsplit(' ', 1)
     price = int(price)
     items[name] = items.get(name, 0) + price
-
 for item, total in items.items():
     print(item, total)
